329c5b008f8544dcd72ce4c05fe36e7e75f61a12
KAC224 <khadijah.aamir.ch@gmail.com>
2026-03-05 15:30:03 +0500

    test updates

--- app/src/test/java/com/example/listycity/CityListTest.java
+++ app/src/test/java/com/example/listycity/CityListTest.java
@@ -16,6 +16,30 @@ class CityListTest {
     }
 
     @Test
+    void testAdd() {
+        CityList cityList = mockCityList();
+        assertEquals(1, cityList.getCities().size());
+
+        City city = new City("Regina", "Saskatchewan");
+        cityList.add(city);
+
+        assertEquals(2, cityList.getCities().size());
+        assertTrue(cityList.getCities().contains(city));
+    }
+
+    @Test
+    void testAddException() {
+        CityList cityList = mockCityList();
+
+        City city = new City("Yellowknife", "Northwest Territories");
+        cityList.add(city);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            cityList.add(city);
+        });
+    }
+
+    @Test
     void testHasCity(){
         CityList cityList = mockCityList();
         City city = mockCity();
@@ -35,12 +59,15 @@ class CityListTest {
 
     @Test
     void testDeleteException(){
+
         CityList cityList = mockCityList();
-        City city = new City("Toronto","Ontario");
+
+        City city = new City("Toronto", "Ontario");
 
         assertThrows(IllegalArgumentException.class, () -> {
             cityList.delete(city);
         });
+
     }
 
     @Test
